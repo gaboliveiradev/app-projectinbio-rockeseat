2b11ac805c987a90c9b32668cb4fbafa8fae46be
gaboliveiradev <g38789138@gmail.com>
2025-08-19 20:08:00 -0300

    feat: create a user card

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,10 +1,11 @@
 import Button from "../button";
+import UserCard from "../commons/user-card";
 import TextInput from "../text-input";
 
 export default function Hero() {
     return (
-        <div className="flex border">
-            <div className="w-full flex flex-col gap-2 mt-[35vh] border">
+        <div className="flex h-screen">
+            <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">Seus projetos e redes sociais em um único link</h1>
                 <h2 className="text-xl leading-6">Crie sua própria página de projetos e compartilhe eles com o mundo.
                     <br />
@@ -16,9 +17,9 @@ export default function Hero() {
                     <Button>Criar agora</Button>
                 </div>
             </div>
-            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
+            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
                 <div className="relative">
-                    {/* <UserCard /> */}
+                    <UserCard />
                     <div className="absolute -bottom[7%] -right-[45%]">
                         {/* <TotalVisits /> */}
                     </div>
