d60052e16f6c144efade0051dc756e5fe8bda970
gaboliveiradev <g38789138@gmail.com>
2025-08-20 20:10:04 -0300

    feat: create project card, header, pricing section, video explanation section, config.ts and faq component section

---
+++ app/components/commons/project-card.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+export default function ProjectCard() {
+    return (
+        <div className="w-[430px] h-[132px] flex gap-5 bg-[#0F0F10] p-3 rounded-[20px] border border-transparent hover:border-[#323234]">
+            <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
+                <img src="/project1.jpg" alt="Projeto" className="w-full h-full object-cover" />
+            </div>
+            <div className="flex flex-col gap-2">
+                <span className="uppercase text-xs font-bold tetxt-[#87BB2D]">10 Cliques</span>
+                <div className="flex flex-col">
+                    <span className="text-white font-bold">Projeto 1</span>
+                    <span className="text-[#CDCBCC] text-sm">Descrição super detalhada do que o projeto faz.</span>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ app/components/landing-page/video-explanation.tsx
@@ -0,0 +1,9 @@
+import { Play } from "lucide-react";
+
+export default function VideoExplanation() {
+    return (
+        <div className="border rounded-2xl border-[#19191A] aspect-video flex items-center justify-center my-20">
+            <Play className="text-[#4B2DBB] size-16" />
+        </div>
+    )
+}
